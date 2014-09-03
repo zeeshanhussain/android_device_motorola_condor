5628a2002a54309af7edd013c2cbb8153488dafa
percy-g2 <gahlotpercy@gmail.com>
2014-09-03 17:53:32 +0530

    Initial fully working multirom source push
    Signed-off-by: zeeshanhussain <zeeshanhussain12@gmail.com>
    
    Conflicts:
    	BoardConfig.mk
    	README.md
    	multirom.dependencies

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,14 +49,148 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 an
 
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 
-# Recovery
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
-BOARD_SUPPRESS_EMMC_WIPE := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
-#TWRP
-DEVICE_RESOLUTION := 540x960
+
+
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
+# Power
+TARGET_POWERHAL_VARIANT := qcom
+
+#TARGET_QCOM_DISPLAY_VARIANT := caf-new
+#TARGET_USES_QCOM_BSP := true
+
+BOARD_USES_QCOM_HARDWARE := true
+
+# Enable WEBGL in WebKit
+ENABLE_WEBGL := true
+
+COMMON_GLOBAL_CFLAGS += -DMOTOROLA_UIDS -DQCOM_HARDWARE
+TARGET_USES_MOTOROLA_LOG := true
+TARGET_NR_SVC_SUPP_GIDS := 32
+
+BOARD_EGL_CFG := $(LOCAL_PATH)/egl.cfg
+
+TARGET_USES_ION := true
+USE_OPENGL_RENDERER := true
+
+# Wifi
+BOARD_HAS_QCOM_WLAN              := true
+BOARD_HAS_QCOM_WLAN_SDK          := true
+BOARD_WLAN_DEVICE                := qcwcn
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_qcwcn
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME          := "wlan"
+TARGET_USES_WCNSS_CTRL           := true
+
+TARGET_QCOM_AUDIO_VARIANT := caf
+BOARD_USES_ALSA_AUDIO := true
+AUDIO_FEATURE_DEEP_BUFFER_PRIMARY := true
+AUDIO_FEATURE_DYNAMIC_VOLUME_MIXER := true
+AUDIO_FEATURE_DISABLED_FM := true
+
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+    $(LOCAL_PATH)/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    adbd.te \
+    app.te \
+    bluetooth_loader.te \
+    bridge.te \
+    camera.te \
+    device.te \
+    dhcp.te \
+    dnsmasq.te \
+    domain.te \
+    drmserver.te \
+    file_contexts \
+    file.te \
+    hostapd.te \
+    init_shell.te \
+    init.te \
+    libqc-opt.te \
+    mediaserver.te \
+    mpdecision.te \
+    netd.te \
+    netmgrd.te \
+    nfc.te \
+    property_contexts \
+    property.te \
+    qcom.te \
+    qmux.te \
+    radio.te \
+    rild.te \
+    rmt.te \
+    sdcard_internal.te \
+    sdcardd.te \
+    sensors.te \
+    shell.te \
+    surfaceflinger.te \
+    system.te \
+    tee.te \
+    te_macros \
+    thermald.te \
+    ueventd.te \
+    vold.te \
+    wpa_supplicant.te \
+    zygote.te
+
+ifneq ($(TARGET_BUILD_VARIANT),user)
+    BOARD_SEPOLICY_UNION += su.te
+endif
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+BLUETOOTH_HCI_USE_MCT := true
+
+# Partition information
 TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 10526720
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10526720
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 29267830784
+BOARD_CACHEIMAGE_PARTITION_SIZE := 734003200
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
+
+BOARD_USES_QC_TIME_SERVICES := true
+
+# Asserts
+TARGET_OTA_ASSERT_DEVICE := xt1021,xt1022,xt1023,condor_umts,condor_umtsds,condor
+
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_msm
+TARGET_LIBINIT_DEFINES_FILE := $(LOCAL_PATH)/init/init_condor.c
+TARGET_UNIFIED_DEVICE := true
+
+# Vold
+BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_VOLD_MAX_PARTITIONS := 40
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+
+# Recovery
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/twrp.fstab:recovery/root/etc/twrp.fstab
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/fstab.qcom
+TARGET_RECOVERY_INITRC := $(LOCAL_PATH)/recovery/init.rc
+
+# TWRP
+DEVICE_RESOLUTION := 540x960
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_RECOVERY_SWIPE := true
+BOARD_SUPPRESS_EMMC_WIPE := true
 RECOVERY_SDCARD_ON_DATA := true
 TW_INTERNAL_STORAGE_PATH := "/data/media"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
@@ -65,3 +199,29 @@ TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_NO_USB_STORAGE := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_IGNORE_MAJOR_AXIS_0 := true
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_IGNORE_MAJOR_AXIS_0 := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TW_IGNORE_MAJOR_AXIS_0 := true
+TW_EXCLUDE_SUPERSU := true
+HAVE_SELINUX := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_NO_SCREEN_TIMEOUT := true
+COMMON_GLOBAL_CFLAGS += -DTW_NO_SCREEN_TIMEOUT
+
+# MultiROM
+MR_INPUT_TYPE := type_b
+MR_INIT_DEVICES := device/motorola/condor/mr_init_devices.c
+MR_DPI := hdpi
+MR_DPI_FONT := 160
+MR_KEXEC_MEM_MIN := 0x05000000
+MR_KEXEC_DTB := true
+MR_CONTINUOUS_FB_UPDATE := true
+MR_FSTAB := $(LOCAL_PATH)/rootdir/twrp.fstab
+MR_USE_MROM_FSTAB := true
+
+PRODUCT_BOOT_JARS := $(subst $(space),:,$(PRODUCT_BOOT_JARS))
+
+-include vendor/motorola/condor/BoardConfigVendor.mk
+
---
+++ README.md
@@ -0,0 +1,23 @@
+MultiROM Device Tree For Motorola Moto E
+========================================
+
+The Motorola (codenamed _"Condor"_) is a low-range smartphone from Motorola mobility.
+
+It was announced on May 2014.
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+CPU     | Dual-core 1.2 GHz Cortex-A7
+CHIPSET | Qualcomm Snapdragon 200
+GPU     | Adreno 302
+Memory  | 1GB RAM
+Shipped Android Version | 4.4.2
+Storage | 4GB
+MicroSD | Upto 32 GB
+Battery | 1980 mAh
+Display | 4.91 x 2.55 x 0.48 in
+Camera  | 5 MP, 2592 х 1944 pixels
+
+![Motorola E](http://3.bp.blogspot.com/-Dn2Rid5a-bs/U1fD6aOx_pI/AAAAAAAAAfM/B-xm4G7uX88/s1600/motorola-moto-e.jpg "Motorola E")
+
+This branch is for building ![MultiROM](https://camo.githubusercontent.com/e42713da5c9fe1210dfe38dc5faf8774b6df3f77/68747470733a2f2f7261772e6769746875622e636f6d2f5461737373616461722f6d756c7469726f6d5f70726f6d6f2f6d61737465722f72656e646572732f647261676f6e5f66756c6c2d707265766965772e706e67 "MultiROM").
