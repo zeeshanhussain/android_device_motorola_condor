ff5df9c1baa41bb63cc04e2711790dcd16e5ad89
Ashwin Ramesh <ashwinr64@gmail.com>
2015-06-30 18:23:16 +0530

    Carbonize

--- carbon.mk
+++ carbon.mk
@@ -1,8 +1,8 @@
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_condor
+PRODUCT_NAME := carbon_condor
 PRODUCT_RELEASE_NAME := MOTO E
